77bcf0594e6b5c7dcbf9c44b3f2da4e1b4c570ec
De Villiers Neethling <dneethling@gew.co.za>
2019-11-06 17:03:46 +0200

    Changes to READMEs and to source code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Zindi
 My source code for [Zindi](https://zindi.africa/) competitions.
+## My Competition Entries
+These are in order of appearance.
+1. [Uber Movement SANRAL Cape Town Challenge](https://github.com/DevilEars/Zindi/tree/master/Uber%20Movement%20SANRAL%20Cape%20Town%20Challenge)
--- Uber Movement SANRAL Cape Town Challenge/uberMovement.py
+++ Uber Movement SANRAL Cape Town Challenge/uberMovement.py
@@ -40,7 +40,10 @@ train = train.loc[train['Occurrence Local Date Time'] >= '2017-01-01']
 local_test = data.loc[data['Occurrence Local Date Time'] < '2019-01-01']
 local_test = local_test.loc[local_test['Occurrence Local Date Time'] >= '2018-09-01']
 
+
+
 # $$$ Reshaping $$$
+
 # Create a dataframe with a column for each segment_id (sid)
 # Each row represents an hour.
 
@@ -58,3 +61,40 @@ for sid in sids:
     dates = dts.astype(str).unique()
     tr.loc[tr['datetime'].isin(dates), sid] = 1
 tr.head()
+
+# Reshape this as in sample submission
+# I add some extra columns that may be useful
+train = pd.DataFrame({
+    'datetime x segment_id':np.concatenate([[str(x) + " x " + str(c) 
+                                             for c in sids] 
+                                            for x in tr['datetime']]),
+    'datetime':np.concatenate([[str(x) for c in sids] for x in tr['datetime']]),
+    'segment_id':np.concatenate([[str(c) for c in sids] for x in tr['datetime']]),
+    'y':tr[sids].values.flatten()
+})
+train.head()
+
+
+# Same for local test
+dts = pd.date_range('2018-09-01','2018-12-31',
+                    freq="1h")
+tr = pd.DataFrame({'datetime':dts})
+
+for sid in sids:
+    tr[str(sid)] = 0
+    events = local_test.loc[local_test['road_segment_id'] == sid]
+    dts = events['Occurrence Local Date Time'].dt.round('H')
+    dates = dts.astype(str).unique()
+    tr.loc[tr['datetime'].isin(dates), sid] = 1
+    
+test = pd.DataFrame({
+    'datetime x segment_id':np.concatenate([[str(x) + " x " + str(c) 
+                                             for c in sids] 
+                                            for x in tr['datetime']]),
+    'datetime':np.concatenate([[str(x) for c in sids] for x in tr['datetime']]),
+    'segment_id':np.concatenate([[str(c) for c in sids] for x in tr['datetime']]),
+    'y':tr[sids].values.flatten()
+})
+test.head()
+# I get 14 234, whereas the notebook gets 14 235. Once fewer accident I suppose?
+train.y.sum()
