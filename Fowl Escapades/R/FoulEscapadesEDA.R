ea0da9ea440412cde6a8a3021858d8d53c5a8657
devilears <devilliersneethling@gmail.com>
2020-03-09 19:57:33 +0200

    Updated my EDA in R, added the CSV which is just for illustration of normal EDA in R, added a single MP3 for explording wav and spectrogram plots in R with the aid of the av package

--- Fowl Escapades/R/FoulEscapadesEDA.R
+++ Fowl Escapades/R/FoulEscapadesEDA.R
@@ -2,28 +2,21 @@
 
 # A blog post that accompanies this code: 
 
-# install packages if needed
+# install if needed
 #install.packages("av")
-
-# import libraries
+# import regardless
 library(av)
-
-# working directory, set as needed
-path <- "~/R/Zindi/Fowl Escapades"
-
-# set working directory
-setwd(path) 
 
 
 
 # $$$$$$ Load datasets $$$$$$
 
-# this dataset just contains a handful of samples
+# This dataset just contains a handful of samples
 # I have placed the samples in their own zip file
-# you need to download that file and extract it for any
-# of this to work
-file_name <- "EDASamples.csv"
-eda_samples <- read.csv(file_name, header = FALSE)
+# you need to download that file and extract it
+# to .data/EDASamples/ for any of this to work
+file_name <- "./csv/EDASamples.csv"
+eda_samples <- read.csv(file_name)
   
 
 
@@ -35,36 +28,32 @@ eda_samples <- read.csv(file_name, header = FALSE)
 # peek inside
 dim(eda_samples)
 
-# check the variables and their types
-str(eda_samples)
-
-# check for null values
+# check for null values. decide what to do with them at this point
 table(is.na(eda_samples))
-
-summary(eda_samples)
 
 
 
 # $$$$$$ Audio Signals EDA $$$$$$
 
 # EDA in time domain
-# TODO fix file path once I have samples 
-audio_file <- "~/R/Zindi/Fowl Escapades/EDASamples/ds5B591U.mp3"
+# I just used a single file for clarity and for saving space
+audio_file <- "./data/EDASamples/ds5B591U.mp3"
 
 
 # show some audio information with the av package
 av_media_info(audio_file)
 
-# read 3 sec of data and plot the waveform
+# read 3 seconds of data and plot the waveform with lines instead of dots
 pcm_data <- read_audio_bin(audio_file, channels = 1, end_time = 3.0)
 plot(pcm_data, type = 'l')
 
 # look at some metrics, some EDA yay!
 dim(pcm_data)
-str(pcm_data)
+# minimum, .., Median, Mean, .. max. that sort of thing
+# this is all on amplitude samples of raw audio
 summary(pcm_data)
 
-# read 3 sec of data and directly transform to frequency
+# read 3 seconds of data and directly transform to time-frequency domain
 # plot the spectrogram
 fft_data <- read_audio_fft(audio_file, end_time = 3.0)
 plot(fft_data)
@@ -74,22 +63,16 @@ dim(fft_data)
 str(fft_data)
 
 
-# EDA in frequency domain
-# from:  https://www.cs.tut.fi/sgn/arg/intro/basics.html
-#u = s(0.2 * fs:0.2 * fs + 511) .* hanning(512);
-#U = fft(u);
-#f = (0:256) / 256 * fs / 2;
-#plot(f, 20 * log10(abs(U(1:257))))
-
-# FFT on bird call
-
-# plot the transformed data
-
-# look at some metrics
-
-
 
 # $$$$$$ Windows and filters $$$$$$
-# There are many more avaiable windows and filters in the signals package
+# I used a tukey window. You can play around with
+# the values, with 256 being a minimum window size
+
+tukey_filter_data <- read_audio_fft(audio_file, end_time = 3.0, tukey(512))
+# look at some metrics
+dim(tukey_filter_data)
+str(tukey_filter_data)
+plot(tukey_filter_data)
+
 
 
